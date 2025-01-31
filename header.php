bf1887f7dceee0a1afdef36275d7bbb950741515
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-01-31 12:26:59 -0300

    Update header.php

--- header.php
+++ header.php
@@ -113,7 +113,7 @@ try {
     <!-- Incluindo o Bootstrap 4 CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Incluindo o CSS customizado -->
-    <link rel="stylesheet" href="./src/style/menu-lateral.css">
+    <link rel="stylesheet" href="src/style/menu-lateral.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
